ca4ff5aacc728c9e737337f0e1fe02e76196a2f6
Harun Özceyhan <harunozceyhan@smartiys.com>
2020-01-03 02:59:10 +0300

    feat: rtl added

--- src/components/common/layout/Settings.vue
+++ src/components/common/layout/Settings.vue
@@ -10,7 +10,7 @@
 		<v-list subheader dense>
 			<v-subheader>{{ $t('base.label.language') }}</v-subheader>
 			<v-list-item>
-				<v-list-item-action style="margin-right: 0"><v-select v-model="language" @change="onLanguageChanged" dense hide-details="" item-color="secondary" color="secondary" :items="languages" :label="$t('base.label.select-language')" return-object outlined></v-select></v-list-item-action>
+				<v-list-item-action style="min-width: 100%"><v-select v-model="language" @change="onLanguageChanged" dense hide-details="" item-color="secondary" color="secondary" :items="languages" :label="$t('base.label.select-language')" return-object outlined></v-select></v-list-item-action>
 			</v-list-item>
 			<v-subheader>{{ $t('base.label.view') }}</v-subheader>
 			<v-list-item
@@ -32,6 +32,25 @@
 					></v-switch>
 				</v-list-item-action>
 			</v-list-item>
+			<v-list-item
+				@click="
+					rtl = !rtl
+					setRtl()
+				"
+			>
+				<v-list-item-title>{{ $t('base.label.rtl') }}</v-list-item-title>
+				<v-list-item-action>
+					<v-switch
+						v-model="rtl"
+						class="ma-2"
+						dense
+						@click="
+							rtl = !rtl
+							setRtl()
+						"
+					></v-switch>
+				</v-list-item-action>
+			</v-list-item>
 		</v-list>
 
 		<v-footer color="accent" :absolute="true">
@@ -46,6 +65,7 @@
 export default {
 	data: () => ({
 		darkMode: null,
+		rtl: null,
 		language: { id: 'tr', text: 'Türkçe' },
 		items: [
 			{ active: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
@@ -63,13 +83,19 @@ export default {
 	mounted() {
 		this.language = localStorage.language === undefined ? { id: 'tr', text: 'Türkçe' } : localStorage.language === 'tr' ? { id: 'tr', text: 'Türkçe' } : { id: 'en', text: 'English' }
 		this.darkMode = localStorage.dark === undefined ? false : localStorage.dark === 'true' ? true : false
+		this.rtl = localStorage.rtl === undefined ? false : localStorage.rtl === 'true' ? true : false
 		this.setTheme()
+		this.setRtl()
 		this.setLanguage()
 	},
 	methods: {
 		setTheme() {
 			this.$vuetify.theme.dark = this.darkMode
 			localStorage.dark = this.darkMode
+		},
+		setRtl() {
+			this.$vuetify.rtl = this.rtl
+			localStorage.rtl = this.rtl
 		},
 		setLanguage() {
 			this.$vuetify.lang.current = this.language.id
